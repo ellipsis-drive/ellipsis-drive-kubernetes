4dbfca01f209bd8ac5ab1dead6ac77878ac16026
NathanErkamp <nathan@ellipsis-drive.com>
2026-03-27 10:37:01 +0100

    test

---
+++ ellipsisDrive/test.js
@@ -0,0 +1,10 @@
+const ellipsis = require('./ellipsisCluster');
+const loadConfig = require('./loadConfig');
+
+test();
+
+async function test(config) {
+  let config = loadConfig();
+  
+  // await ellipsis.createCluster(config);
+}
