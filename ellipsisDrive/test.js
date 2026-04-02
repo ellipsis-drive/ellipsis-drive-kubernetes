c76988b968c78d83adb7e365d8ffc1bd861fe4d3
NathanErkamp <nathan@ellipsis-drive.com>
2026-04-02 10:43:54 +0200

    test

--- ellipsisDrive/test.js
+++ ellipsisDrive/test.js
@@ -27,8 +27,8 @@ async function test() {
 
   // await ellipsis.createOwl(config);
   // await ellipsis.createPigeon(config);
-  await ellipsis.createEmu(config);
-  // await ellipsis.createAlbatross(config);
+  // await ellipsis.createEmu(config);
+  await ellipsis.createAlbatross(config);
   // await ellipsis.createRooster(config);
   // await ellipsis.createPenguin(config);
 
